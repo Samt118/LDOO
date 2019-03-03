c2a3163f08055f7e96ed92d88f6e1fdc4879caab
Samt118 <47702770+Samt118@users.noreply.github.com>
2019-03-02 22:27:01 -0600

    Add files via upload

---
+++ RegistroUsuarios.java
@@ -0,0 +1,68 @@
+package Practica2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/RegistroUsuarios")
+public class RegistroUsuarios extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public RegistroUsuarios() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter salida=response.getWriter();
+		
+		salida.println("<html><body>");
+		salida.println("<h1 style='text-align: center'>Registro</h1>");
+		salida.println("<br></br>");
+		
+		salida.println("Nombre introducido: " + request.getParameter("nombre"));
+		salida.println("<br></br>");
+		
+		salida.println("Email introducido: " + request.getParameter("email"));
+		salida.println("<br></br>");
+		
+		salida.println("Contraseña introducida: " + request.getParameter("contraseña"));
+		salida.println("<br></br>");
+		
+		salida.println("Esta es la fecha de nacimiento: " + request.getParameter("fecha"));
+		salida.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+PrintWriter salida=response.getWriter();
+		
+		salida.println("<html><body>");
+		salida.println("<h1 style='text-align: center'>Registro</h1>");
+		salida.println("<br></br>");
+		
+		salida.println("Nombre introducido: " + request.getParameter("nombre"));
+		salida.println("<br></br>");
+		
+		salida.println("Email introducido: " + request.getParameter("email"));
+		salida.println("<br></br>");
+		
+		salida.println("Contraseña introducida: " + request.getParameter("contraseña"));
+		salida.println("<br></br>");
+		
+		salida.println("Esta es la fecha de nacimiento: " + request.getParameter("fecha"));
+		salida.println("</body></html>");
+	}
+
+}
